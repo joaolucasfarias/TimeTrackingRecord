ada51de995bd2e598f014334de6891d4db15c223
João Lucas Farias <joao@sistemasbr.net>
2018-01-01 18:44:47 -0200

    Model to salary values

---
+++ Models/SalaryValuesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    class SalaryValuesModel
+    {
+        public decimal Month { get; set; }
+        public decimal Hour { get; set; }
+        public decimal Overtime { get; set; }
+    }
+}
